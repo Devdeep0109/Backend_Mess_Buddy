e1cadb98390297f2c1787a2ca01ed71c60b8d9d3
Devdeep0109 <devdeepgoyal@gmail.com>
2025-05-06 13:20:31 +0530

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(
 
 //middleware.....
 app.use(express.json());
-app.use(express.static('uploads')); // Serve static files from the "public" directory
+app.use(express.static('public/uploads')); // Serve static files from the "public" directory
 app.use(express.urlencoded({ extended: false }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
